0507445877a7aec5b8876d3c04332cd11f94c52b
DmitryKrom <krom77@hotmail.de>
2025-07-16 22:23:19 +0200

    check ssh again

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-	// Checking github
+	// Checking github ssh again
 	mongoConn := options.Client().ApplyURI("mongodb://127.0.0.1:27017/")
 	ctx := context.Background()
 	client, err := mongo.Connect(ctx, mongoConn)
